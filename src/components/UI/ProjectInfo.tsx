1b11de247e7c29836692389b2253296bf27135d6
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-04-01 07:59:58 -0700

    project info

--- src/components/UI/ProjectInfo.tsx
+++ src/components/UI/ProjectInfo.tsx
@@ -16,14 +16,17 @@ const ProjectInfo = ({
   secondChild,
 }: ProjectInfo) => {
   return (
-    <div className="tiny:flex tiny:flex-col md:flex-row-reverse md:justify-center md:items-center">
-      <div className="w-full bg-[#111214] tiny:min-h-[244px] smaller:min-h-[304px] sm:min-h-[384px] md:min-h-[340px] px-4 py-8 overflow-hidden">
+    <div className="tiny:flex tiny:flex-col md:flex-row-reverse md:justify-center md:items-center pc:max-w-[1287px] pc:mx-auto pc:pl-[141px] 2xl:pl-0">
+      <div
+        className="w-full bg-[#111214] tiny:min-h-[244px] smaller:min-h-[304px] sm:min-h-[384px] md:min-h-[340px] lg:min-h-[360px] xl:min-h-[420px]
+       px-4 md:px-0 md:pl-4 lg:pl-8 xl:pl-[62px] pc:pl-[86px] tiny:py-8 xl:py-[62px] overflow-hidden"
+      >
         <div
-          className={`w-full tiny:min-h-[180px] smaller:min-h-[240px] sm:min-h-[320px] md:min-h-[240px] relative tiny:rounded-[22px] md:rounded-none overflow-hidden ${
+          className={`w-full tiny:min-h-[180px] smaller:min-h-[240px] sm:min-h-[320px] md:min-h-[240px] relative tiny:rounded-[22px] md:rounded-none  overflow-hidden ${
             secondChild ? "md:rotate-[-10deg] md:ml-[28px]" : ""
-          }`}
+          } lg:min-h-[320px] xl:min-h-[360px]`}
         >
-          <div className="absolute inset-0 rounded-[22px] border-[3px] border-transparent bg-gradient-to-r from-[#333] via-transparent to-[#333] p-[3px] overflow-hidden">
+          <div className="absolute inset-0 tiny:rounded-[22px] md:rounded-none md:rounded-tl-[22px] md:rounded-bl-[22px] border-[3px] border-transparent bg-gradient-to-r from-[#333] via-transparent to-[#333] p-[3px] overflow-hidden">
             <Image
               src={`${image}`}
               alt="spiderman"
